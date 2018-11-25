778f76c2abbd0546007a582ec89bc7c7cdf43c98
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-11-25 18:32:51 +0000

    Add first iteration of example func

--- src/r3x.ts
+++ src/r3x.ts
@@ -6,17 +6,17 @@ let path = require('path')
 
 
 // todo handle user function
-export function execute(r3x: void, scheme: any) {
+export function execute(r3x: Function, scheme: any) {
 }
 
 
-function HTTPStream(r3x: void, scheme: any) {
+function HTTPStream(r3x: Function, scheme: any) {
 
     let functionHandler = (req: RequestOptions, res: ServerResponse) => {
     }
 
     let server = http.createServer(functionHandler).listen(8080)
-    
+
     return () => {
         server
     }
