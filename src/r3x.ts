cf374052e75bbd349d038261ecf0e533b8f9b61f
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-11-26 10:23:29 +0000

    Add JSON handler

--- src/r3x.ts
+++ src/r3x.ts
@@ -1,4 +1,5 @@
-import { ServerResponse, RequestOptions } from "http";
+import { ServerResponse, RequestOptions, ServerRequest } from "http";
+import { JSONHandler } from './handlers/JSONhandler';
 
 let fs = require('fs')
 let http = require('http')
@@ -7,12 +8,23 @@ let path = require('path')
 
 // todo handle user function
 export function execute(r3x: Function, scheme: any) {
+    // todo add error handler
+    HTTPStream(r3x, scheme)
 }
 
 
 function HTTPStream(r3x: Function, scheme: any) {
 
-    let functionHandler = (req: RequestOptions, res: ServerResponse) => {
+    let functionHandler = (req: ServerRequest, res: ServerResponse) => {
+        let input = new JSONHandler()
+
+        req.on('data', chunk => {
+            input.pushData(chunk)
+        }).on('end', () => {
+            let headers = {}
+            let rewHeaders = req.rawHeaders
+
+        })
     }
 
     let server = http.createServer(functionHandler).listen(8080)
