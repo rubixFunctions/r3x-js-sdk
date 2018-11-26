5487f58be049b42bfe03faa374facee492ecd88f
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-11-26 11:11:25 +0000

    Add handle res struct

--- src/hello-r3x.ts
+++ src/hello-r3x.ts
@@ -1,8 +1,10 @@
 import * as r3x from './r3x'
 
-let scheme 
 
+let scheme
 r3x.execute(function(){
     let response = {'message' : 'Hello r3x function'}
     return response 
 }, scheme)
+
+
--- src/r3x.ts
+++ src/r3x.ts
@@ -6,30 +6,56 @@ let http = require('http')
 let path = require('path')
 
 
-// todo handle user function
+// handle user function
 export function execute(r3x: Function, scheme: any) {
+    console.log("r3x Execute Fired")
     // todo add error handler
     HTTPStream(r3x, scheme)
 }
 
-
+// handle http strea,
 function HTTPStream(r3x: Function, scheme: any) {
-
     let functionHandler = (req: ServerRequest, res: ServerResponse) => {
+        console.log("Server Hit")
         let input = new JSONHandler()
-
+    
         req.on('data', chunk => {
             input.pushData(chunk)
         }).on('end', () => {
             let headers = {}
             let rewHeaders = req.rawHeaders
 
+            let body = input.getBody()
+
+            new Promise(function (res, rej) {
+                try {
+                    return res(r3x(body))
+                } catch (err) {
+                    rej(err)
+                } 
+            }).then((result) => {
+                return sendResponse(res, result)
+            }, (error) => {
+
+            })
         })
     }
 
     let server = http.createServer(functionHandler).listen(8080)
 
     return () => {
+        console.log("Running")
         server
     }
 } 
+
+
+// handle response
+function sendResponse(resp : any, result : any){
+    console.log("hello bah")
+}
+
+// handle error
+function sendError(resp : any, code : any, error : any){
+
+}
